a5376ebc279bf1825177fe45ae73e69180ddb418
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:09:03 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+// Diketahui segitiga siku-siku ABC dengan siku-siku di B digambarkan sebagai berikut :
+
+
+
+// Tentukan panjang sisi miring AC pada gambar di atas dengan menggunakan kode program javascript dan wajib menggunakan fungsi POW dan SQRT. Lalu buatlah tampilan output seperti gambar berikut ini :
+
+function mathe(){
+    var ac = Math.sqrt((Math.pow(8,2))+(Math.pow(6,2)));
+    console.log(ac);
+}
+mathe();
